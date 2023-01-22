9f8420042c35276139da62fbc1c405e07e33ad24
Yevhenii009 <stifgun@gmail.com>
2023-01-22 16:27:02 +0200

    дз11-1.правки7

--- src/js/news-servise.js
+++ src/js/news-servise.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const BASE_URL = 'https://pixabay.com/api';
-const API_KEY = '25261319-41493d7d09d351884ef55fa82';
+const API_KEY = '32997819-c25a939264b27cca8c9e33adb';
 
 
 export default class NewsApiService {
@@ -14,7 +14,7 @@ export default class NewsApiService {
 
 async makesRequest () {
         const params = new URLSearchParams({
-            key: API_KEY,   //ключ IPI 
+            key: API_KEY,
             q: this.searchQuery,
             image_type: 'photo',
             orientation: 'horizontal',
@@ -27,39 +27,17 @@ async makesRequest () {
         return await axios.get(url);
     }; 
 
-    
-//     featchArticles(page) {
-//        console.log(this);
-//     try {
-//        const url = await axios.get(
-//         `${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.pageAmount}&per_page=${this.perPage}',);
-//        const images = await response.data;
-//        return images;
-//         } catch (error) {
-//       console.log(error);
-//    }
-    
-//    return fetch(
-//     `${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${pageAmount}`,
-//     )
-//     .then(response => response.json())
-//     .then(console.log);
-//   };
-
-
-
     incrementPage() {
-        this.page += 1;           //Добавление сл.страницы/увеличение
+        this.page += 1;
     }
 
     resetPage() {
-        this.page = 1;            //След.поиск с 1 стр./сброс
+        this.page = 1;
     }
 
     get query() {
         return this.searchQuery;
     }
-                                                //Контролирует термин запроса
     set query(newQuery) {
         this.searchQueary = newQuery;
     }
